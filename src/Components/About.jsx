1bfa9849d258dfd146ecc97ced04d0d0e80d9b96
Ivan-mwaura <94759966+Ivan-mwaura@users.noreply.github.com>
2023-02-02 17:09:08 +0300

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,24 @@ const About= () =>{
     return(
         <div>
             <div>
-                
+                <div>
+                    <p>
+                        Hello My name is Evans Mwaura abd i enjoy creating thg things that live on the <br/>
+                        internet My interest in web development started back in 2021 when I started to <br/>
+                        try editing custom Tumbelr themes - turns out hacking together a custom reblog <br/>
+                        taught me a lot about HTML & CSS!
+                        <br/>
+                        <br/>
+                        Fast-forward to today, I have continued bulding my expertize and upgrading my <br/>
+                        by doing multiple projects which have enabled me to master my skill. My main <br/>
+                        focus today is building inclusive products and digital experinces for a variety <br/>
+                        of clients.
+
+                        Here are a few technologies I`ve been working on recently
+
+                        
+                    </p>
+                </div>
             </div>
         </div>
     )
